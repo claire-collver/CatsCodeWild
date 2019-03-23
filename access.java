84f3272fc31dedf75264a38fef025fbe52640c66
Ana Hayne <anhayne@davidson.edu>
2019-03-23 06:02:56 -0400

    updated

--- access.java
+++ access.java
@@ -13,12 +13,48 @@ import java.util.*;
  public class access {
 
    //zip code --> total num
-   private static HashMap<Integer, Integer> cchcVisits = new HashMap<Integer, Integer>();
+   private static HashMap<Integer, Integer> zipVisits = new HashMap<Integer, Integer>();
 
    //zip code, lat/long
     private static HashMap<Integer, Double[]> zips = new HashMap<Integer, Double[]>();
 
     private static HashMap<Double, Integer> distanceVisits = new HashMap<Double, Integer>();
+
+    //private static HashMap<Integer, Double> zipPoverty = new HashMap<Integer, Double>();
+
+    private static HashMap<Integer, Integer> tractToZip = new HashMap<Integer, Integer>();
+
+    public static void readInTractToZip() throws FileNotFoundException, IOException {
+        //change this file
+        BufferedReader reader = new BufferedReader(new FileReader("ZipToTract.csv"));
+
+        reader.readLine(); //skip line 1
+        String line = reader.readLine();
+
+        while (line != null && !line.isEmpty()) {
+
+            String[] fields = line.split(",");
+            try {
+
+              int zipcode = Integer.valueOf(fields[0].trim());
+              int tract = Integer.valueOf(fields[1].trim());
+              System.out.println()
+
+              if(! tractToZip.containsKey(tract)){
+                tractToZip.put(tract, zipcode);
+              }
+
+
+              }
+
+            catch (NumberFormatException e) {
+            }
+            line = reader.readLine();
+    }
+        reader.close();
+    }
+
+
 
 
     public static void readInZipCodes() throws FileNotFoundException, IOException {
@@ -26,11 +62,12 @@ import java.util.*;
 
         String line = reader.readLine();
         while (line != null && !line.isEmpty()) {
-			String[] fields = line.split(",");
+			      String[] fields = line.split(",");
             int zipcode = Integer.valueOf(fields[0].trim());
             Double[] coords = new Double[2];
             coords[0] = Double.valueOf(fields[1].trim()); //lat
             coords[1] = Double.valueOf(fields[2].trim()); //lng
+
             zips.put(zipcode, coords);
             line = reader.readLine();
 		}
@@ -38,21 +75,29 @@ import java.util.*;
     }
 
 
-	public static double distanceToClinic(double clinicLat, double clinicLong, int zip) {
+	public static double distanceToClinic( int zipCode) {
+
 
     double R = 3963.1676;
+    double clinicLat = 35.228700;
+    double clinicLong = -80.920360;
+
 
 		double phi1 = Math.toRadians(clinicLat);
-		double phi2 = Math.toRadians(zips.get(zip)[0]);
+		double phi2 = Math.toRadians(zips.get(zipCode)[0]);
 		double pi1 = Math.toRadians(clinicLong);
-		double pi2 = Math.toRadians(zips.get(zip)[1]);
-		double distance = (R * (    Math.acos(   (Math.sin(phi1)*Math.sin(phi2)) +
+		double pi2 = Math.toRadians(zips.get(zipCode)[1]);
+		double distance = (R * ( Math.acos( (Math.sin(phi1)*Math.sin(phi2)) +
 				(Math.cos(phi1)*Math.cos(phi2)*Math.cos(pi1 - pi2)))));
+
 		return distance;
+
+
     }
 
-    public static void readInCCHC() throws FileNotFoundException, IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("CCHC.csv"));
+    public static void readInCSV() throws FileNotFoundException, IOException {
+        //change this file
+        BufferedReader reader = new BufferedReader(new FileReader("MedAssist.csv"));
 
         reader.readLine(); //skip line 1
         String line = reader.readLine();
@@ -61,9 +106,19 @@ import java.util.*;
             String[] fields = line.split(",");
             try {
               int zipcode = Integer.valueOf(fields[0].trim());
-              int totalFemale = Integer.valueOf(fields[1].trim());
-              int totalMale = Integer.valueOf(fields[2].trim());
-              cchcVisits.put(zipcode, totalFemale + totalMale);
+
+              if(zips.containsKey(zipcode)){
+                int totalAA = Integer.valueOf(fields[1].trim());
+                int totalAI = Integer.valueOf(fields[2].trim());
+                int totalA = Integer.valueOf(fields[3].trim());
+                int totalC = Integer.valueOf(fields[4].trim());
+                int totalH = Integer.valueOf(fields[5].trim());
+                int totalOther = Integer.valueOf(fields[6].trim());
+                int sum = totalAA + totalAI + totalA + totalC + totalH + totalOther;
+                zipVisits.put(zipcode, sum);
+
+              }
+
             }
             catch (NumberFormatException e) {
             }
@@ -74,20 +129,13 @@ import java.util.*;
 
     public static void calculateDistances(){
 
-      double clinicLat1 = 35.319114;
-      double clinicLong1 = -80.776729;
-      double clinicLat2 = 35.232695;
-      double clinicLong2 = -80.92763;
 
-      for (Integer i: cchcVisits.keySet()) {
-        double d1 = distanceToClinic(clinicLat1, clinicLong1, i);
-        double d2 = distanceToClinic(clinicLat2, clinicLong2, i);
-        if(d1 <= d2){
-          distanceVisits.put(d1, cchcVisits.get(i));
-        }
-        else{
-          distanceVisits.put(d2, cchcVisits.get(i));
-        }
+      for (Integer i: zipVisits.keySet()) {
+
+        double d = distanceToClinic(i);
+
+        distanceVisits.put(d, zipVisits.get(i));
+
 
       }
 
@@ -96,19 +144,17 @@ import java.util.*;
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
-      readInCCHC();
-      for (Integer i: cchcVisits.keySet()) {
-          System.out.println("ZIP: " + i + ", Total Visits: " + cchcVisits.get(i));
+      readInTractToZip();
+
+      for(Integer i : tractToZip.keySet()){
+        System.out.println(i + " zip: " + tractToZip.get(i));
       }
+      //
+      // readInZipCodes();
+      //
+      // readInCSV();
+      //
+      // calculateDistances();
 
-        readInZipCodes();
-        for (Integer i: zips.keySet()) {
-            System.out.println("ZIP: " + i + ", LAT: " + zips.get(i)[0] + ", LNG: " + zips.get(i)[1]);
-        }
-
-        calculateDistances();
-        for(Double i: distanceVisits.keySet()){
-              System.out.println("DISTANCE: " + i + " miles, Total Visits: " + cchcVisits.get(i));
-        }
     }
  }
